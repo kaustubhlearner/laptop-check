dd1e22f4eaec2164c219ad50d793b5d71fa62cd7
kaustubhlearner <kaustubhpandey81788>
2024-11-18 15:36:53 +0530

    firstcodeoflaptop

---
+++ script.js
@@ -0,0 +1,27 @@
+// let a= 96
+// let b= 23
+// console.log(a+b);
+
+// let a =[4,54,23,9,34]
+let sq = a.map(a=> a**2)
+console.log(sq);
+
+
+let a =() =>{
+    return "hello word"
+}
+
+console.log(a());
+
+
+let arr = [34,54,23,90]
+let updatedarr= arr.map(arr => arr+2)
+console.log(updatedarr);
+
+
+// let arr = [34,54,23,90]
+// let updatedarr= arr.filter(arr => arr>23)
+// console.log(updatedarr);
+
+
+console.log(2+3+ "5");
